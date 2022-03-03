3b08c95357b6da03d3dd397829a48c2d45bc3e1a
Avoler0 <jys1391@gmail.com>
2022-03-03 20:58:06 +0900

    Home Screen Part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,12 +1,71 @@
 import { useQuery } from "react-query";
+import styled from "styled-components";
 import { getMovies } from "../api";
+import { makeImagePath } from "../utils";
 
+const Wrapper = styled.div`
+  background: black;
+`;
 
+interface IMovie {
+  id:number,
+  backdrop_path:string,
+  poster_path:string,
+  title:string,
+  overview:string,
+}
 
+export interface IGetMoviesResult {
+  dates:{
+    maximum: string;
+    minimum: string;
+  }
+  page: number,
+  results: IMovie[],
+  total_pages: number,
+  total_results: number,
+}
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom:10px;
+`;
+const Overview = styled.div`
+  font-size:36px;
+  width:50%;
+`;
 function Home() {
-  const {data,isLoading} = useQuery(["movies" ,"nowPlaying"], getMovies);
+  const {data,isLoading} = useQuery<IGetMoviesResult>(["movies" ,"nowPlaying"], getMovies);
   console.log(data,isLoading);
   
-  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
+  return <Wrapper style={{ backgroundColor: "whitesmoke", height: "200vh" }}>{isLoading ? <Loader></Loader> : <>
+    <Banner 
+      bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+      <Title>
+        {data?.results[0].title}
+      </Title>
+      <Overview>
+        {data?.results[0].overview}
+      </Overview>
+    </Banner>
+    </>}
+    </Wrapper>;
 }
 export default Home;
