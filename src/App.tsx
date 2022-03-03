95eb7a768492304b7e5ebc782b4c8a31b05759da
Avoler0 <jys1391@gmail.com>
2022-03-03 18:14:42 +0900

    Header Part Three

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <BrowserRouter>
     <Header/>
-        <Route path="/">
-          <Home />
-        </Route>
         <Route path="/tv">
           <Tv />
         </Route>
         <Route path="/search">
           <Search />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
     </BrowserRouter>
   )
