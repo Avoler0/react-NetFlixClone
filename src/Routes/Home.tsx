83b5ebd2fa80eb2d0e659938e9903d7054a29dd4
Avoler0 <jys1391@gmail.com>
2022-03-04 16:24:51 +0900

    Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,36 +1,19 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
-import { getMovies } from "../api";
+import { motion, AnimatePresence } from "framer-motion";
+import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background: black;
 `;
 
-interface IMovie {
-  id:number,
-  backdrop_path:string,
-  poster_path:string,
-  title:string,
-  overview:string,
-}
-
-export interface IGetMoviesResult {
-  dates:{
-    maximum: string;
-    minimum: string;
-  }
-  page: number,
-  results: IMovie[],
-  total_pages: number,
-  total_results: number,
-}
-
 const Loader = styled.div`
   height: 20vh;
   display: flex;
   justify-content: center;
-  align-items:center;
+  align-items: center;
 `;
 
 const Banner = styled.div<{ bgPhoto: string }>`
@@ -43,29 +26,88 @@ const Banner = styled.div<{ bgPhoto: string }>`
     url(${(props) => props.bgPhoto});
   background-size: cover;
 `;
+
 const Title = styled.h2`
   font-size: 68px;
-  margin-bottom:10px;
+  margin-bottom: 20px; ;
 `;
-const Overview = styled.div`
-  font-size:36px;
-  width:50%;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
 `;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
 function Home() {
-  const {data,isLoading} = useQuery<IGetMoviesResult>(["movies" ,"nowPlaying"], getMovies);
-  console.log(data,isLoading);
-  
-  return <Wrapper style={{ backgroundColor: "whitesmoke", height: "200vh" }}>{isLoading ? <Loader></Loader> : <>
-    <Banner 
-      bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-      <Title>
-        {data?.results[0].title}
-      </Title>
-      <Overview>
-        {data?.results[0].overview}
-      </Overview>
-    </Banner>
-    </>}
-    </Wrapper>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 export default Home;
