f27740691986ac5c7246acc262968ca27e61efeb
Avoler0 <jys1391@gmail.com>
2022-03-06 14:44:43 +0900

    Movie Modal part Three

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -123,7 +123,31 @@ const BigMovie = styled(motion.div)`
   left: 0;
   right: 0;
   margin: 0 auto;
-  background-color:red;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color:${(props) => props.theme.black.lighter};
+
+`;
+const BigCover = styled.img`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+
+`;
+
+const BigTitle = styled.h3`
+  color:${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position:relative;
+  top: -130px;
+`;
+const BigOverview = styled.p`
+  padding:20px;
+  color:${(props) => props.theme.white.lighter};
+  position:relative;
+  top: -80px;
 `;
 const Overlay = styled(motion.div)`
   position:fixed;
@@ -133,6 +157,7 @@ const Overlay = styled(motion.div)`
   background-color: rgba(0,0,0,0.5);
   opacity:0;
 `;
+
 
 function Home() {
   const history = useHistory();
@@ -161,6 +186,9 @@ function Home() {
     history.push(`/movies/${movieId}`);
   }
   const onOverlayClick = () => history.push("/");
+  const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find(movie => movie.id === +bigMovieMatch.params.movieId);
+  console.log(clickedMovie);
+  
   return (
     <Wrapper>
       {isLoading ? (
@@ -210,7 +238,15 @@ function Home() {
             <BigMovie
             style={{ top:scrollY.get()+100 }}
             layoutId={bigMovieMatch.params.movieId+""} 
-            > hello </BigMovie> </>) : null}
+            >
+              {clickedMovie && <>
+                <BigCover 
+                  style={{backgroundImage: `linear-gradient(to top,black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})`}}
+                   />
+                <BigTitle>{clickedMovie.title}</BigTitle>
+                <BigOverview>{clickedMovie.overview}</BigOverview>
+              </>}
+            </BigMovie> </>) : null}
           </AnimatePresence>
         </>
       )}
